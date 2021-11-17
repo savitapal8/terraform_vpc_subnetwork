f66bb55d5186854adbea4855911d42ded0b2c94c
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-11-17 11:57:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,5 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   project                 = "my-project-name"
   name                    = "vpc-network"
-  auto_create_subnetworks = false
   mtu                     = 1460
-  delete_default_routes_on_create = true
 }
